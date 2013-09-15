6ec4bade088c1811e13b554ad5f1b1302e54aede
Ken Faulkner <ken.faulkner@gmail.com>
2013-09-15 19:10:04 +1000

    initial

---
+++ PorterStemmerLib/PorterStemmer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PorterStemmerLib
+{
+    public class PorterStemmer
+    {
+
+        public void StemFile(string contents)
+        {
+            
+            while (true)
+            {
+                int ch = getc(f);
+                if (ch == EOF) return;
+                if (LETTER(ch))
+                {
+                    int i = 0;
+                    while (TRUE)
+                    {
+                        if (i == i_max) increase_s();
+
+                        ch = tolower(ch); /* forces lower case */
+
+                        s[i] = ch; i++;
+                        ch = getc(f);
+                        if (!LETTER(ch)) { ungetc(ch, f); break; }
+                    }
+                    s[stem(s, 0, i - 1) + 1] = 0;
+                    /* the previous line calls the stemmer and uses its result to
+                       zero-terminate the string in s */
+                    printf("%s", s);
+                }
+                else putchar(ch);
+            }
+        }
+
+    }
+}
---
+++ porterstemmer/PorterStemmer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace porterstemmer
+{
+    class PorterStemmer
+    {
+
+        List<string> targetStringList = new List<string>();
+
+        void Step1ab(string word)
+        {  
+            // last letter of word is 's'
+            if (word[ word.Length -1] == 's')
+            {  
+                if (ends("\04" "sses")) k -= 2; else
+                if (ends("\03" "ies")) setto("\01" "i"); else
+                if (b[k-1] != 's') k--;
+            }
+           if (ends("\03" "eed")) { if (m() > 0) k--; } else
+           if ((ends("\02" "ed") || ends("\03" "ing")) && vowelinstem())
+           {  k = j;
+              if (ends("\02" "at")) setto("\03" "ate"); else
+              if (ends("\02" "bl")) setto("\03" "ble"); else
+              if (ends("\02" "iz")) setto("\03" "ize"); else
+              if (doublec(k))
+              {  k--;
+                 {  int ch = b[k];
+                    if (ch == 'l' || ch == 's' || ch == 'z') k++;
+                 }
+              }
+              else if (m() == 1 && cvc(k)) setto("\01" "e");
+           }
+        }
+
+        string Stem(string word)
+        {
+            var word1 = Step1ab( word ); 
+            var word2 = Step1c( word1 ); 
+            var word3 = Step2(word2); 
+            Step3(); 
+            Step4(); 
+            Step5();
+            return k;
+
+        }
+
+        internal void StemFile(string filename)
+        {
+            // need to replace string concat.
+            var resultingString = "";
+
+            using (TextReader reader = File.OpenText(filename))
+            {
+                var line = reader.ReadLine();
+
+                var sp = line.Split();
+                foreach (var word in sp)
+                {
+                    // if all letters, then continue.
+                    // LINQ...  slow?
+                    var allLetters = word.All(x => Char.IsLetter(x));
+                    if (allLetters)
+                    {
+                        var resultWord = Stem(word);
+                        targetStringList.Add(resultWord);
+                    }
+                    else
+                    {
+                        targetStringList.Add(word);
+                    }
+
+                }
+            }
+        }
+
+    }
+}
